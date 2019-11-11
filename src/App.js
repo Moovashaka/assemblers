2ad9de75019efd4a7c28d4a82a94ee32ebb9da6b
Moova Shaka <mrmoovashaka@gmail.com>
2019-11-11 17:48:56 +0000

    Styling Navbar cont.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './assemblerslogo.svg';
-import './App.css';
+// import logo from './assemblerslogo.svg';
+// import './App.css';
 import Navbar from './components/navigation/Navbar.js'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
--- src/components/navigation/DesktopNavbar.js
+++ src/components/navigation/DesktopNavbar.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
-import logo from 'assemblerslogo.svg'
+import logo from './assemblerslogo.svg'
 import styled from 'styled-components'
 
 
 const MyDesktopNavbar = styled.nav`
-  display:flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-
-  background: red;
-  color: white;
-
-  height:15vh;
+background-color: #282c34;
+min-height: 30vh;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: top;
+font-size: calc(10px + 2vmin);
+color: white;
+`
+const Img = styled.img `
+   height: 20vmin;
+   float: left;
 `
 
 const DesktopNavbar = () => {
     return (
       <MyDesktopNavbar>
-      <div className="logo ">
-      <img src={logo} alt="logo" />
-      </div>
+      <Img src={logo} />
       </MyDesktopNavbar>
     );
   }
