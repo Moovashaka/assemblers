1763cbac968da5788b8a8b67f5fcc93a6608d07b
Moova Shaka <mrmoovashaka@gmail.com>
2019-11-11 18:41:45 +0000

    added nav links

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 // import logo from './assemblerslogo.svg';
 // import './App.css';
 import Navbar from './components/navigation/Navbar.js'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <BrowserRouter>
         <Navbar />
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
-    </div>
+    </BrowserRouter>    
+
+
   );
 }
 
--- src/components/navigation/DesktopNavbar.js
+++ src/components/navigation/DesktopNavbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from './assemblerslogo.svg'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 
 const MyDesktopNavbar = styled.nav`
@@ -12,6 +13,28 @@ align-items: center;
 justify-content: top;
 font-size: calc(10px + 2vmin);
 color: white;
+
+.nav-links {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+
+  List-style:none;
+  
+  width: 35vw;
+}
+
+.link {
+  color: white;
+  font-size: 2.5vh;
+  text-decoration: none;
+}
+`
+const MyMobileNavButton = styled.button`
+  background: transparent;
+  height: 6vh;
+
 `
 const Img = styled.img `
    height: 20vmin;
@@ -23,6 +46,21 @@ const DesktopNavbar = () => {
     return (
       <MyDesktopNavbar>
       <Img src={logo} />
+      <ul className="nav-links">
+          <li>
+            <Link to="/" className="link"></Link>
+          </li>
+          <li>
+            <Link to="/about" className="link">About</Link>
+          </li>
+          <li>
+            <Link to="/feedback" className="link">Feedback</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="link">Contact</Link>
+          </li>
+        </ul>
+
       </MyDesktopNavbar>
     );
   }
