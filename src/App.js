12efbd250686010461bd6814d2a75528e27d889d
Moova Shaka <mrmoovashaka@gmail.com>
2019-11-12 19:44:29 +0000

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 // import logo from './assemblerslogo.svg';
 // import './App.css';
 import Navbar from './components/navigation/Navbar.js'
+import Footer from './components/main/Footer.js'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
         <Navbar />
-    </BrowserRouter>    
+        <Footer />
+    </BrowserRouter>
 
 
   );
