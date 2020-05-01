f5d79f231b615578d004b704f6f1765bedf83c63
Moova Shaka <mrmoovashaka@gmail.com>
2020-05-01 17:04:39 +0100

    adjusting contact form position for mobile

--- src/components/main/Form.js
+++ src/components/main/Form.js
@@ -97,7 +97,7 @@ export default class ContactForm extends React.Component {
 
   <section id="contact">
         <h3 className="header">Contact Us</h3>
-  <div>
+
     <div className="map">
     <iframe className="mapImage" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302755.07458312873!2d-3.0037013935293686!3d53.64208603536602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b3ee6fcfe1f89%3A0x1ed6fb17951ed48c!2sMarlborough%20Rd%2C%20Southport%20PR9%200RA!5e0!3m2!1sen!2suk!4v1588347466793!5m2!1sen!2suk"/>
 
@@ -161,7 +161,7 @@ export default class ContactForm extends React.Component {
     </form>
     </div>
   </div>
-  </div>
+  
   </section>
 
     );
