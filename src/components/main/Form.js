230dfb1dc1809f66392c412ee53a1b09874b3327
Moova Shaka <mrmoovashaka@gmail.com>
2020-05-04 12:59:02 +0100

    adjusting map size and position

--- src/components/main/Form.js
+++ src/components/main/Form.js
@@ -101,7 +101,6 @@ export default class ContactForm extends React.Component {
     <div className="map">
     <iframe className="mapImage" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302755.07458312873!2d-3.0037013935293686!3d53.64208603536602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b3ee6fcfe1f89%3A0x1ed6fb17951ed48c!2sMarlborough%20Rd%2C%20Southport%20PR9%200RA!5e0!3m2!1sen!2suk!4v1588347466793!5m2!1sen!2suk"/>
 
-        <div className="formSurround">
         <form className="contactForm" onSubmit={ this.handleSubmit }>
           <div className="form-group">
             <input type="hidden" name="form-name" value="contactForm" />
@@ -157,7 +156,6 @@ export default class ContactForm extends React.Component {
         <button className="text-center" id="submit" type="submit" name="dataSubmit" disabled={this.state.lockSubmit}>Submit</button>
         <span>{this.state.formSent}</span>
     </form>
-    </div>
   </div>
 
   </section>
