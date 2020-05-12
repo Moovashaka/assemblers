16c8a8c646517808800ad587dca67499cfbcd5fc
Moova Shaka <mrmoovashaka@gmail.com>
2020-05-12 14:53:44 +0100

    adding async loading to google map

--- src/components/main/Form.js
+++ src/components/main/Form.js
@@ -99,7 +99,7 @@ export default class Form extends React.Component {
         <h3 className="header">Contact Us</h3>
 
     <div className="map">
-    <iframe defer className="mapImage" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302755.07458312873!2d-3.0037013935293686!3d53.64208603536602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b3ee6fcfe1f89%3A0x1ed6fb17951ed48c!2sMarlborough%20Rd%2C%20Southport%20PR9%200RA!5e0!3m2!1sen!2suk!4v1588347466793!5m2!1sen!2suk" />
+    <iframe async className="mapImage" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302755.07458312873!2d-3.0037013935293686!3d53.64208603536602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b3ee6fcfe1f89%3A0x1ed6fb17951ed48c!2sMarlborough%20Rd%2C%20Southport%20PR9%200RA!5e0!3m2!1sen!2suk!4v1588347466793!5m2!1sen!2suk" />
 
         <form className="contactForm" onSubmit={ this.handleSubmit }>
           <div className="form-group">
