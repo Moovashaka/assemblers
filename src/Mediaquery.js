1c2157ab06fa3b95b02501f11c40e17aeedcffad
Moova Shaka <mrmoovashaka@gmail.com>
2020-06-24 18:02:36 +0100

    adding bing validation

--- src/Mediaquery.js
+++ src/Mediaquery.js
@@ -1,18 +1,41 @@
-import React from 'react'
+// import React from 'react'
+// import imgsm from './images/nwmap250x179.jpg'
+// import webpsm from './images/nwmap250x179.webp'
+// import imgmd from './images/nwmap350x250.jpg'
+// import webpmd from './images/nwmap350x250.webp'
+//
+// /* global Modernizr */
+// function switchimg() {
+// let screensizes = {
+//   sm: '(max-width: 360px)',
+//   md: '(max-width: 480px)',
+//   lg: '(min-width: 768px)',
+// };
+// let imgdisp;
+//
+// const query = Modernizr.mq(screensizes);
+//
+// const webpimg = Modernizr.webp;
+// console.log(query)
+//
+//  imgdisp = <img src={webpsm} alt="text here" />;
+//
+//
+//    return (
+//      <div>
+//      imgdisp
+//      </div>
+//    );
+//     }
 
-/* global Modernizr */
+   // } else if
+   //  (!webpimg && query.md){
+   //      imgdisp = imgmd;
+   //   } else {
+   //      imgdisp = imgsm;
+   //   }
+   //   return imgdisp;
+   // };
 
-let screensizes = [
-  sm: ((max-width: 360px)),
-  md: ((max-width: 480px)),
-  lg: ((min-width: 768px))
-];
 
-
-
-const queries = () => {
-  const query = modernizr.mq(screensizes)
-    return query
-}
-
-export default queries
+export default switchimg
--- src/components/main/Map.js
+++ src/components/main/Map.js
@@ -5,7 +5,7 @@ import webpsm from '../../images/nwmap250x179.webp'
 import imgmd from '../../images/nwmap350x250.jpg'
 import webpmd from '../../images/nwmap350x250.webp'
 
-// import queries from '../../Mediaquery.js'
+// import switchimg from '../../Mediaquery.js'
 
 
 /* global Modernizr */
@@ -13,7 +13,7 @@ export default class Map extends React.Component {
 render() {
   return(
       <div className="mapImage">
-      <img src={(Modernizr.webp ? webpsm : imgsm )} alt="North West Furniture Assembly Areas"></img>
+      <img src={webpsm} alt="text here" />
       </div>
     )
   }
